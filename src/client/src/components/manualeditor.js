37d66557c58930c73f6d2787bab612394ea5e3b2
To Tran Nhat Truong <nhattruongtotran@gmail.com>
2021-10-14 18:13:03 +0700

    Update function preview & add note

--- src/client/src/components/manualeditor.js
+++ src/client/src/components/manualeditor.js
@@ -20,40 +20,64 @@ class ManualEditor extends Component {
   
     preview(event) {
       event.preventDefault();
-      console.log("Preview");
+      let text_to_render = document.getElementById("editor_textarea").value;
+
+      text_to_render = text_to_render.replace(/\n/g, "<br>");
+    
+      text_to_render = text_to_render.replace(/<script>/g, "");
+      text_to_render = text_to_render.replace(/<\/script>/g, "");
+    
+      text_to_render = text_to_render.replace(/<link>/g, "");
+      text_to_render = text_to_render.replace(/<\/link>/g, "");
+    
+      text_to_render = text_to_render.replace(/<div>/g, "");
+      text_to_render = text_to_render.replace(/<\/div>/g, "");
+    
+      text_to_render = text_to_render.replace(/<p>/g, "");
+      text_to_render = text_to_render.replace(/<\/p>/g, "");
+    
+      text_to_render = text_to_render.replace(/<span>/g, "");
+      text_to_render = text_to_render.replace(/<\/span>/g, "");
+    
+      text_to_render = text_to_render.replace(/<style>/g, "");
+      text_to_render = text_to_render.replace(/<\/style>/g, "");
+    
+      let render_div = document.getElementById("right_pane");
+    
+      render_div.innerHTML = text_to_render;
     }
   
     insertTag(event, tag_name) {
       event.preventDefault();
       let editor_textarea = document.getElementById("editor_textarea");
   
-      //let selection = null;
-      //
-      //if (editor_textarea.selectionStart === editor_textarea.selectionEnd)
-      //  selection = editor_textarea.selectionStart;
-      //else
-      //  selection = editor_textarea.value.slice(editor_textarea.selectionStart, editor_textarea.selectionEnd);
-      //
-      console.log(tag_name);
-      //switch (tag_name) {
-      //  case "strong":
-      //    tag_name = "strong";
-      //    break;
-      //
-      //  case "italic":
-      //    tag_name = "i";
-      //    break;
-      //
-      //  case "footnote":
-      //    tag_name = "a href";
-      //    break;
-      //  default:
-      //    tag_name = null;
-      //    break;
-      //}
-      //
-      //if (tag_name != null)
-      //  editor_textarea.setRangeText(`<${tag_name}>${selection}</${tag_name}>`);
+      let selection = null;
+      
+      if (editor_textarea.selectionStart === editor_textarea.selectionEnd)
+        selection = editor_textarea.selectionStart;
+      else
+        selection = editor_textarea.value.slice(editor_textarea.selectionStart, editor_textarea.selectionEnd);
+      
+      console.log(selection);
+      switch (tag_name) {
+        case "strong":
+          tag_name = "strong";
+          break;
+      
+        case "italic":
+          tag_name = "i";
+          break;
+      
+        case "footnote":
+          tag_name = "a href";
+          break;
+        default:
+          tag_name = null;
+          break;
+      }
+      
+      if (tag_name != null)
+        editor_textarea.setRangeText(`<${tag_name}>${selection}</${tag_name}>`);
     }
   
     render() {
@@ -80,6 +104,14 @@ class ManualEditor extends Component {
                   onClick={(event) => this.insertTag(event, "italic")}
                 >
                   Italic
+                </button>
+              </div>
+              <div className="inner">
+                <button
+                  type="submit"
+                  onClick={(event) => this.insertTag(event, "footnote")}
+                >
+                  Footnote
                 </button>
               </div>
             </div>
